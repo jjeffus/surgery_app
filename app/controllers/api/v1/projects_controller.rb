84fdb3fb44aff4cbebd43af636106a0d70e423dc
Janet Jeffus <speak@jjeff.us>
2019-02-12 22:50:36 -0600

    updating readme with project research

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ProjectsController < ApplicationController
   def index
     @page = params[:page].to_i
     # @projects = Project.where('english > 0 and category in ("mtf", "ftm") or category is null').paginate(:page => @page)
-    @projects = Project.where('english > 0 and category is null').paginate(:page => @page)
+    @projects = Project.where('english > 0 and category="mtf" and amount > 8000').paginate(:page => @page)
     render 'index'
   end
 
